e27a98ebc52e157b16b3ce93b7fa29e0fea504a0
Razeenmia <rashaad.mia@gmial.com>
2024-10-14 19:44:22 +0200

    git push

---
+++ main.py
@@ -0,0 +1,36 @@
+# List of different types of iPhones
+iphone_models = [
+    "iPhone", "iPhone 3G", "iPhone 3GS", "iPhone 4", "iPhone 4S",
+    "iPhone 5", "iPhone 5C", "iPhone 5S", "iPhone 6", "iPhone 6 Plus",
+    "iPhone 6S", "iPhone 6S Plus", "iPhone SE (1st generation)", "iPhone 7",
+    "iPhone 7 Plus", "iPhone 8", "iPhone 8 Plus", "iPhone X", "iPhone XR",
+    "iPhone XS", "iPhone XS Max", "iPhone 11", "iPhone 11 Pro", "iPhone 11 Pro Max",
+    "iPhone SE (2nd generation)", "iPhone 12", "iPhone 12 Mini", "iPhone 12 Pro",
+    "iPhone 12 Pro Max", "iPhone 13", "iPhone 13 Mini", "iPhone 13 Pro",
+    "iPhone 13 Pro Max", "iPhone SE (3rd generation)", "iPhone 14", "iPhone 14 Plus",
+    "iPhone 14 Pro", "iPhone 14 Pro Max", "iPhone 15", "iPhone 15 Plus",
+    "iPhone 15 Pro", "iPhone 15 Pro Max", "iPhone Lite", "iPhone Max",
+    "iPhone Mini", "iPhone Fold", "iPhone Flex", "iPhone Air", "iPhone Pro Lite",
+    "iPhone Ultra", "iPhone 16", "iPhone 16 Mini", "iPhone 16 Pro", "iPhone 16 Pro Max",
+    "iPhone 17", "iPhone 17 Mini", "iPhone 17 Pro", "iPhone 17 Pro Max",
+    "iPhone 18", "iPhone 18 Mini", "iPhone 18 Pro", "iPhone 18 Pro Max",
+    "iPhone Classic", "iPhone Retro", "iPhone Sport", "iPhone Adventure",
+    "iPhone Adventure Plus", "iPhone Vision", "iPhone Max Pro", "iPhone Max Lite",
+    "iPhone Quantum", "iPhone 20", "iPhone 20 Mini", "iPhone 20 Pro",
+    "iPhone 20 Pro Max", "iPhone 21", "iPhone 21 Mini", "iPhone 21 Pro",
+    "iPhone 21 Pro Max", "iPhone 22", "iPhone 22 Mini", "iPhone 22 Pro",
+    "iPhone 22 Pro Max", "iPhone 23", "iPhone 23 Mini", "iPhone 23 Pro",
+    "iPhone 23 Pro Max", "iPhone 24", "iPhone 24 Mini", "iPhone 24 Pro",
+    "iPhone 24 Pro Max", "iPhone 25", "iPhone 25 Mini", "iPhone 25 Pro",
+    "iPhone 25 Pro Max", "iPhone 26", "iPhone 26 Mini", "iPhone 26 Pro",
+    "iPhone 26 Pro Max", "iPhone 27", "iPhone 27 Mini", "iPhone 27 Pro",
+    "iPhone 27 Pro Max", "iPhone 28", "iPhone 28 Mini", "iPhone 28 Pro",
+    "iPhone 28 Pro Max", "iPhone 29", "iPhone 29 Mini", "iPhone 29 Pro",
+    "iPhone 29 Pro Max", "iPhone 30", "iPhone 30 Mini", "iPhone 30 Pro",
+    "iPhone 30 Pro Max"
+]
+
+# Print the list of iPhone models
+for iPhone in iphone_models:
+    print(iPhone)
+    
